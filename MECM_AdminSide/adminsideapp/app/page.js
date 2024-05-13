c38833a8244efee9aac6d8c85ba81b7ac6290e21
Ayush Anshuman Supakar <supakarayush@gmail.com>
2024-05-14 01:04:16 +0530

    6th commit : next app created

---
+++ MECM_AdminSide/adminsideapp/app/page.js
@@ -0,0 +1,12 @@
+import { getDisplayName } from "next/dist/shared/lib/utils";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div >
+      <h1>Ayush's App</h1>
+    </div>
+    </>
+  );
+}
