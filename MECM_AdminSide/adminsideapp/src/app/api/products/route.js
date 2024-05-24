df959d87a33f2ea46dd505c26f3fec6dcb8fe79c
Ayush Anshuman Supakar <supakarayush@gmail.com>
2024-05-24 19:17:08 +0530

    added different pages, designed products page, new products page  and profile page, integrated mongoose and axios to add and fetch products from data base

--- MECM_AdminSide/adminsideapp/src/app/api/products/route.js
+++ MECM_AdminSide/adminsideapp/src/app/api/products/route.js
@@ -3,14 +3,21 @@ import { NextResponse } from "next/server";
 import { mongooseConnect } from '../../../../lib/mongooseConnect';
 import { Product } from '../../../../lib/models/Product';
 
-
+ await mongooseConnect();
 export async function GET(req) {
-  return NextResponse.json({message : "Hello World"});
+ 
+  const allproducts = await Product.find();
+  console.log(allproducts);
+  return NextResponse.json(allproducts);
 }
 export async function POST(req, res){
-  await mongooseConnect();
+ 
   const body = await req.json();
   console.log(body);
   await Product.create(body);
   return NextResponse.json({message : "Hello post"});
+}
+export async function DELETE(req, res){
+  const did = await req.json()
+  console.log("about to delete this mf = " +did );
 }
--- MECM_AdminSide/adminsideapp/src/app/products/page.js
+++ MECM_AdminSide/adminsideapp/src/app/products/page.js
@@ -1,10 +1,42 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import MyLayout from "../../../components/MyLayout"
 import Link from 'next/link'
+import axios from 'axios';
 export default function page() {
+  const[allproducts,setallproducts ] = useState([]);
+  useEffect(()=>{
+    axios.get('/api/products').then(response=>{ setallproducts(response.data); });
+
+  },[]);
+   async function Delete(pname){
+    //await axios.delete('api/products', {pid});
+    console.log("Deleting : " + pname);
+  }
   return (
     <MyLayout>
         <Link href="/products/new" className='bg-blue-900 text-white p-2 px-4 rounded-lg'>Add New Products</Link>
+
+        <div className="flex">
+          <table>
+            <thead>
+              <tr>
+                <td className="text-center border-2 border-blue-900 " >Products</td>
+                <td className="text-center border-2 border-blue-900 ">Delete</td>
+              </tr>
+            </thead>
+            <tbody>
+              {allproducts.map(eachproduct => {
+                return (
+                  <tr className="border-2 border-blue-900 " key={eachproduct._id}>
+                    <td className=" text-center p-2 border-2 border-blue-900 " >{eachproduct.prdname}</td>
+                    <td className=" flex items-center justify-center p-2 "><div className="flex items-center justify-center rounded-lg p-2 px-4 font-bold bg-blue-900 text-white"><button className='' onClick={ev=>Delete(eachproduct.prdname)}>DELETE</button></div></td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
     </MyLayout>
   )
 }
